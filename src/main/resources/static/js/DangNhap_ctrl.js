61f4107fe0f6b6b15cad076df403f43486bdc97f
Hưng Lê <hungltps31968@fpt.edu.vn>
2024-10-30 11:08:20 +0700

    Sửa chức năng đăng ký

--- src/main/resources/static/js/DangNhap_ctrl.js
+++ src/main/resources/static/js/DangNhap_ctrl.js
@@ -1,6 +1,7 @@
 let host = "http://localhost:8080";
-var app = angular.module('loginApp', []);
-app.controller('loginController', function($scope, $http, $window) {
+var appDangNhap = angular.module('loginApp', []);
+appDangNhap.controller('loginController', function ($scope, $http, $window) {
+
     $scope.login = function() {
 
         // Tạo FormData để gửi dữ liệu dạng form-urlencoded
@@ -8,17 +9,27 @@ app.controller('loginController', function($scope, $http, $window) {
         formData.append('identifiers', $scope.identifiers);
         formData.append('password', $scope.password);
 
-        $http.post('http://localhost:8080/api/user/dang-nhap', formData, {
+        $http.post(`${host}/api/user/dang-nhap`, formData, {
             transformRequest: angular.identity, // Không serialize dữ liệu
             headers: {'Content-Type': undefined} // Để browser tự set Content-Type
         }).then(function successCallback(response) {
             var user = response.data; // Lấy User từ response
 
-            $http.post('http://localhost:8080/login-success', user).then(function() {
-                $window.location.href = 'http://localhost:8080/index'; // Chuyển hướng đến /index
+            $http.post(`${host}/login-success`, user).then(function () {
+                $window.location.href = `${host}/index`; // Chuyển hướng đến /index
             });
         }).catch((error) => {
             console.log("Error", error);
+        });
+    };
+
+    $scope.logout = function () {
+        $http.post(`${host}/api/user/dang-xuat`)
+            .then(function successCallback(response) {
+                console.log(response); // In ra thông báo đăng xuất thành công
+                $window.location.href = `${host}/logout`; // Redirect đến /logout
+            }).catch((error) => {
+                console.log("Error", error); // Xử lý lỗi nếu có
         });
     };
 });
